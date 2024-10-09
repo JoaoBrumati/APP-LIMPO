b0a2bc32ad1dd41de3b267733042e85b46245625
Auto Logon <autologon@CSED.LAB>
2024-10-09 19:58:47 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+const App = () => {
+  const [conversionRate, setConversionRate] = useState(null);
+  const [result, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      try {
+        const response = await axios.get('https://open.er-api.com/v6/latest/USD');
+        setConversionRate(response.data.rates);
+      } catch (error) {
+        console.error("Erro ao buscar a taxa de câmbio:", error);
+      }
+    };
+
+    fetchExchangeRate();
+  }, []);
+
+  const calculate = () => {
+    const valueToConvert = parseFloat(inputValue);
+    if (conversionRate && !isNaN(valueToConvert)) {
+      const convertedValue = valueToConvert * conversionRate['EUR'];
+      setResult(convertedValue);
+    } else {
+      setResult(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Taxa de Câmbio (USD para EUR): {conversionRate ? conversionRate['EUR'] : 'Carregando...'}
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o valor em USD"
+        keyboardType="numeric"
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
+      <Button title="Calcular" onPress={calculate} />
+      {result !== null && <Text style={styles.text}>Resultado da conversão: {result.toFixed(2)} EUR</Text>}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    width: '100%',
+    paddingHorizontal: 10,
   },
 });
+
+export default App;
